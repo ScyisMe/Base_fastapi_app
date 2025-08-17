448065b4a32fd44c1e4c99350099d8c6a8593223
Mykola <150530049+Bonyjairs@users.noreply.github.com>
2025-08-17 22:55:49 +0300

    Create database stratagy

--- fastapi-application/api/dependancy/strategy.py
+++ fastapi-application/api/dependancy/strategy.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:
     from core.model import AccessToken
     
 from core.config import setting
+from .access_token import get_access_token_db
 
 def get_database_strategy(
     access_token_db: Annotated[AccessTokenDatabase[AccessToken], Depends(get_access_token_db)],
