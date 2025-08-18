854fb44229d723b9353e83c0facb229f7eea2658
Mykola <mikolamelnicuk132@gmail.com>
2025-08-18 12:47:23 +0300

    move dependancy inner packege

--- fastapi-application/api/dependancy/authentication/fastapi_users.py
+++ fastapi-application/api/dependancy/authentication/fastapi_users.py
@@ -2,7 +2,7 @@ from core.types.user_id import UserIdType
 from fastapi_users import FastAPIUsers
 
 from .depends_user_maneger import get_user_manager
-from api.dependancy.bakend import authenticationbackend
+from .bakend import authenticationbackend
 from core.model.user import User
 
 fastapi_users = FastAPIUsers[User, UserIdType](
